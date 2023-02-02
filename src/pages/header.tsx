9222e71d729ceb72a9e679f3d849f8b455b1d352
smmartprogrammer <partabrajp@gmail.com>
2023-02-03 03:17:05 +0500

    updating name tag

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -3,6 +3,8 @@ import React from "react";
 export default function Header() {
   return (
     <div className="bg-violet-800 text-white text-center py-36 ">
+        <p className="mt-3">Pricing UI using Tailwind CSS - by Partab Raj.</p>
+
       <div>
         <h1 className="text-4xl font-bold">Simple pricing for your business</h1>
       </div>
